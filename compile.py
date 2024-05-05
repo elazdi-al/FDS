31b8a87cfbd5d558bacf3cdf426893683004f1ab
Ali <ali.elazdi@gmail.com>
2024-05-05 17:48:45 +0200

    latest

--- compile.py
+++ compile.py
@@ -1,8 +1,9 @@
 import subprocess
 def compile_latex_to_pdf(latex_file):
     try:
-        result = subprocess.run(["pdflatex", latex_file], check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(latex_file)
+        subprocess.run(["pdflatex", latex_file], check=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print("Compilation successful, output written to PDF.")
-        print(result.stdout)
+        
     except:
         print("Failed to compile LaTeX file. Error details:")
